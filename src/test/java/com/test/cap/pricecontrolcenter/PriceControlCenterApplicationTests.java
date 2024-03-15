a92ef0f79f7dd35e3652d838ea4d51ee0e396d43
ruben-furlan <rubenfurlan@gmail.com>
2024-03-15 16:30:07 -0300

    Primer commit - estructura inicial

---
+++ src/test/java/com/test/cap/pricecontrolcenter/PriceControlCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cap.pricecontrolcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceControlCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
