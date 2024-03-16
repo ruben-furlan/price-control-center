42530c5fb44a168c6d8ccd5d09c21fd701aacda4
ruben-furlan <rubenfurlan@gmail.com>
2024-03-16 08:12:39 -0300

    - Le doy un poco de formato a la entity - Me gusta mas los campos en los getters (gusto personal)

--- src/main/java/com/test/cap/pricecontrolcenter/infraestructure/out/entity/PricesEntity.java
+++ src/main/java/com/test/cap/pricecontrolcenter/infraestructure/out/entity/PricesEntity.java
@@ -1,8 +1,7 @@
 package com.test.cap.pricecontrolcenter.infraestructure.out.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,78 +20,97 @@ public class PricesEntity {
     private Integer priority;
     private BigDecimal price;
     private String currency;
+    private Long version;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return this.id;
+    }
+
+    @Column(name = "BRAND_ID")
+    public Integer getBrandId() {
+        return this.BrandId;
+    }
+
+    @Column(name = "START_DATE")
+    public LocalDateTime getStartDate() {
+        return this.startDate;
+    }
+
+    @Column(name = "END_DATE")
+    public LocalDateTime getEndDate() {
+        return this.endDate;
+    }
+
+    @Column(name = "PRICE_LIST")
+    public Integer getPriceList() {
+        return this.priceList;
+    }
+
+    @Column(name = "PRODUCT_ID")
+    public Integer getProductId() {
+        return this.productId;
+    }
+
+    @Column(name = "PRIORITY")
+    public Integer getPriority() {
+        return this.priority;
+    }
+
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    @Column(name = "CURRENCY")
+    public String getCurrency() {
+        return this.currency;
+    }
+
+    @Column(name = "VERSION")
+    public Long getVersion() {
+        return this.version;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Integer getBrandId() {
-        return this.BrandId;
-    }
-
     public void setBrandId(Integer brandId) {
         this.BrandId = brandId;
-    }
-
-    public LocalDateTime getStartDate() {
-        return this.startDate;
     }
 
     public void setStartDate(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 
-    public LocalDateTime getEndDate() {
-        return this.endDate;
-    }
-
     public void setEndDate(LocalDateTime endDate) {
         this.endDate = endDate;
-    }
-
-    public Integer getPriceList() {
-        return this.priceList;
     }
 
     public void setPriceList(Integer priceList) {
         this.priceList = priceList;
     }
 
-    public Integer getProductId() {
-        return this.productId;
-    }
-
     public void setProductId(Integer productId) {
         this.productId = productId;
-    }
-
-    public Integer getPriority() {
-        return this.priority;
     }
 
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
 
-    public BigDecimal getPrice() {
-        return this.price;
-    }
-
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
-    public String getCurrency() {
-        return this.currency;
-    }
-
     public void setCurrency(String currency) {
         this.currency = currency;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 
 }
