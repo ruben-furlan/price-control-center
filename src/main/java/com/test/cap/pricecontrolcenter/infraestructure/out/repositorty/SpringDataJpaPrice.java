343dada4115a4c4ba3f50bd4223e13eadfdf7ce1
ruben-furlan <42449800+ruben-furlan@users.noreply.github.com>
2024-03-16 12:02:14 -0300

    Revert "Metemos lombok y agregamos mappers de entridad a modelo y de modelo a nuestra entidad"

--- src/main/java/com/test/cap/pricecontrolcenter/infraestructure/out/repositorty/SpringDataJpaPrice.java
+++
@@ -1,9 +0,0 @@
-package com.test.cap.pricecontrolcenter.infraestructure.out.repositorty;
-
-import com.test.cap.pricecontrolcenter.infraestructure.out.entity.PricesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpringDataJpaPrice  extends JpaRepository<PricesEntity, Long> {
-}
