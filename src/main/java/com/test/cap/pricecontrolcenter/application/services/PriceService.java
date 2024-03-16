284f11fd0c5511e5289e4f9e62b6eb76c87b177a
ruben-furlan <rubenfurlan@gmail.com>
2024-03-16 15:13:56 -0300

    feature -5 continui con la persistencia y creo estructura de controller, casos de usos y services

---
+++ src/main/java/com/test/cap/pricecontrolcenter/application/services/PriceService.java
@@ -0,0 +1,14 @@
+package com.test.cap.pricecontrolcenter.application.services;
+
+import com.test.cap.pricecontrolcenter.domain.model.PriceModel;
+import com.test.cap.pricecontrolcenter.domain.port.in.PriceUserCase;
+
+public class PriceService implements PriceUserCase {
+
+    @Override
+    public PriceModel create(PriceModel priceModel) {
+
+        return null;
+    }
+
+}
