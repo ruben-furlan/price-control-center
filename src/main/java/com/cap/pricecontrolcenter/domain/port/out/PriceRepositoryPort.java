9c9d796f3b5c0e3a716074ca55feede4bca8cb1b
ruben-furlan <rubenfurlan@gmail.com>
2024-03-19 09:35:20 -0300

    Feature-16 un poco de javadoc y comentarios em el pom

--- src/main/java/com/cap/pricecontrolcenter/domain/port/out/PriceRepositoryPort.java
+++ src/main/java/com/cap/pricecontrolcenter/domain/port/out/PriceRepositoryPort.java
@@ -5,11 +5,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * Interfaz que define los puertos para la manipulación de precios en el repositorio.
+ */
 public interface PriceRepositoryPort {
 
+    /**
+     * Guarda un modelo de precio en el repositorio.
+     *
+     * @param price El modelo de precio a guardar.
+     * @return Un objeto Optional que puede contener el modelo de precio guardado, si es exitoso, o vacío si falla.
+     */
     Optional<PriceModel> save(PriceModel price);
 
-    Optional<List<PriceModel>>  findByDateProductAndBrandOrderByPriorityDesc(LocalDateTime applicationDate, Integer productId, Integer brandId);
-
-
+    /**
+     * Busca modelos de precio para una fecha de aplicación, producto y marca específicos, ordenados por prioridad .
+     *
+     * @param applicationDate La fecha en la que se desea aplicar el precio.
+     * @param productId       El identificador del producto.
+     * @param brandId         El identificador de la marca.
+     * @return Un objeto Optional que puede contener una lista de modelos de precio encontrados
+     */
+    Optional<List<PriceModel>> findByDateProductAndBrandOrderByPriorityDesc(LocalDateTime applicationDate, Integer productId, Integer brandId);
 }
